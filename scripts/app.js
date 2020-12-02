ab3a8f2a53956ea7b339dacc82e7e8f6843cc193
nistalb <brent.daddy@gmail.com>
2020-12-02 09:13:31 -0800

    completed getName function

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,8 @@ $("#notice-screen").append($startButton);
 //create the event listener that will tell when the start button is pushed
 $("#start").on("click", function (event){
     console.log("the game is starting")
+    $("#start").remove();
+    getName();
 })
 
 //this will swicth between the game screen and the notice screen
@@ -23,13 +25,21 @@ const switchScreens = function(){
     }    
 }
 
-//this function will collect name
+//this function will collect name and print onto the screen
 let petName = "";
 
 const getName = function getName() {
-    $("#notice-screen").append("<input><button id = 'pet-name'>Enter your pet's name</button></input>");
+    let $name = $("<input><button id = 'pet-name'>Enter your pet's name</button></input>")
+    $("#notice-screen").append($name);
     $("#pet-name").on("click", function(event){
         petName = $("input").val();
+        $($name).remove();
+        $("#notice-screen").append(`Your new pet ${petName} is about to hatch!`);
     });
-    
 };
+
+//this function will create a timer for the various elements of hte game
+
+const timer = function timer(time){
+    
+}
