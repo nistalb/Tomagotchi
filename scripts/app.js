fc5dd36861944195350325bc7d557ebb73f21b11
nistalb <brent.daddy@gmail.com>
2020-12-02 13:14:22 -0800

    refactored decrement functions, added onclick for feeding

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
 
 let babyfood = ["peas", "creamed corn"];
+let hunger = 0;
+let boredom = 0;
+let sleep = 0;
 
 //create the start button that will begin the game and append to the screen
 let $startButton = $("<button id = 'start'>Start Game</button>")
@@ -57,38 +60,61 @@ const incrementHunger = function incrementHunger(hunger){
 }
 
 const decrementHunger = function decrementHunger(hunger){
-    hunger--;
-    $("#hunger").text(hunger);  
+    hunger = hunger - 3;
+    if (hunger > 0) {
+        $("#hunger").text(hunger);
+    } else {
+        hunger = 0;
+        $("#hunger").text(hunger);
+    }
 }
 
 //this function increments sleepiness
 const incrementSleepiness = function incrementSleepiness(sleep){
     sleep++;
-    $("#sleepiness").text(`Sleepiness: ${sleep}`);  
+    $("#sleepiness").text(sleep);  
 }
 
 const decrementSleepiness = function decrementSleepiness(sleep){
-    sleep--;
-    $("#sleepiness").text(`Sleepiness: ${sleep}`);  
+    sleep = sleep - 5;
+    if (sleep > 0) {
+        $("#sleepiness").text(sleep); 
+    } else {
+        sleep = 0;
+        $("#sleepiness").text(sleep);
+    } 
 }
 
 //this function increments boredom
 const incrementBoredom = function incrementBoredom(bored){
     bored++;
-    $("#boredom").text(`Boredom: ${bored}`);  
+    $("#boredom").text(bored);  
 }
 
 const decrementBoredom = function decrementBoredom(bored){
-    bored--;
-    $("#boredom").text(`Boredom: ${bored}`);  
+    bored = bored - 4;
+    if (bored > 0) {
+        $("#boredom").text(bored); 
+    } else {
+        bored = 0;
+        $("#boredom").text(bored);
+    }
 }
 
 
 let time = 120;
-    const setTimer = function setTimer(){
+    const setTimer = function setTimer(hunger, boredom, sleep, interval){
         const updateTime = function updateTime(){
             $("#time").text(time);
             time--;
+            if(time % interval === 0){
+                hunger++
+                boredom++
+                sleep++
+                incrementBoredom(boredom);
+                incrementHunger(hunger);
+                incrementSleepiness(sleep);
+            }
             if (time <= 0){
                 clearInterval(timer);
                 lifecycle++;
@@ -104,23 +130,10 @@ const babyPet = function babyPet() {
     let hunger = 0;
     let boredom = 0;
     let sleep = 0;
+    interval = 5;
    
-    while (time > 0) {
-        const updateStats = function updateStats() {
-            let deltaTime = 120 - time;
-            console.log(deltaTime);
-            if(deltaTime % 5 === 0){
-                hunger++
-                boredom++
-                sleep++
-                incrementBoredom();
-                incrementHunger();
-                incrementSleepiness();
-            }
-        }
-        setInterval(updateStats, 500);
+    setTimer(hunger, boredom, sleep, interval)
     }
-}
 
 let lifecycle = 1;
 
@@ -128,7 +141,11 @@ const playGame = function playGame (){
     if (lifecycle === 1) {
         babyPet ();
         time = 120;
-    }
-    
-    setTimer();
+    }    
 }
+
+//NOTE address my variables issue
+$("#feed").on("click", function (event){
+    console.log("being fed")
+    decrementHunger(hunger);
+})
